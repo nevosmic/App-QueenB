88688ab5f63a3fbc3370a1cbae6f77893d05bf13
nevosmic <nevosmic28@gmail.com>
2020-09-16 00:55:25 +0300

    creat Alert Dialog

--- app/src/main/java/com/example/queenb/InfoF.java
+++ app/src/main/java/com/example/queenb/InfoF.java
@@ -1,17 +1,22 @@
 package com.example.queenb;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,6 +36,7 @@ public class InfoF extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    Button beerSheva;
 
     public InfoF() {
         // Required empty public constructor
@@ -57,39 +63,67 @@ public class InfoF extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        List<InfoNode> myList = initInfo();
-        for(InfoNode myNode : myList){
-            TextView myT=getActivity().findViewById(myNode.idName);
-            String sourceString = "<b>" +myNode.city + "</b> " +myNode.place;
-            //myT.setText(Html.fromHtml(sourceString));
-        }
 
-        //paste here
-//        BeerSheva = getView().findViewById(R.id.BeerSheva);
-//        Jerusalm1 = getView().findViewById(R.id.Jerusalem1);
-//        Jerusalm2 = getView().findViewById(R.id.Jerusalem2);
-//        Jerusalm3 = getView().findViewById(R.id.Jerusalem3);
+    }
+    void showDialog(String myText, Drawable myImg){
+        LayoutInflater inflater = LayoutInflater.from(this.getActivity());
+        View view=inflater.inflate(R.layout.alert_dialog, null);
 
-      //  BeerSheva.setOnClickListener(new View.OnClickListener() {
-      //      @Override
-      //      public void onClick(View view) {
-       //         Intent intent = new Intent(getActivity().getApplication(), MainActivityList.class);
-       //         startActivity(intent);
-        //    }
-     //   });
+        ImageView imageView =(ImageView)view.findViewById(R.id.img);
+        imageView.setImageDrawable(myImg);
 
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        TextView mytext = (TextView)view.findViewById(R.id.mytext) ;
+        mytext.setText(myText);
+
+        AlertDialog alertDialog=new AlertDialog.Builder(this.getActivity())
+                .setView(view)
+                .create();
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+        List<InfoNode> myList = initInfo();
+        for(InfoNode myNode : myList){
+            TextView myT=view.findViewById(myNode.idName);
+            String sourceString = "<b>" +myNode.city + "</b> " +myNode.place;
+            // myT.setText(sourceString);
+            // myT.setText(Html.fromHtml(sourceString));
+        }
+
+        //paste here
+        beerSheva = view.findViewById(R.id.BeerSheva);
+        beerSheva.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = String.valueOf(R.string.text1);
+                Drawable d = getResources().getDrawable(R.drawable.ic_launcher_foreground);
+                showDialog(s,d);
+            }
+        });
+
+//        Jerusalm1 = getView().findViewById(R.id.Jerusalem1);
+//        Jerusalm2 = getView().findViewById(R.id.Jerusalem2);
+//        Jerusalm3 = getView().findViewById(R.id.Jerusalem3);
+
+        //  BeerSheva.setOnClickListener(new View.OnClickListener() {
+        //      @Override
+        //      public void onClick(View view) {
+        //         Intent intent = new Intent(getActivity().getApplication(), MainActivityList.class);
+        //         startActivity(intent);
+        //    }
+        //   });
+
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        return view;
+
     }
     private List<InfoNode> initInfo() {
         List<InfoNode> output = new ArrayList<>();
@@ -107,4 +141,6 @@ public class InfoF extends Fragment {
 
         return output;
     }
+
+
 }
