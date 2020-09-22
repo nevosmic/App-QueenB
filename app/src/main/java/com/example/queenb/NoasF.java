3473a5e88a0d30c398daadee9358bf5a1239cedf
nevosmic <nevosmic28@gmail.com>
2020-09-22 14:47:00 +0300

    Merg with Noa

--- app/src/main/java/com/example/queenb/NoasF.java
+++ app/src/main/java/com/example/queenb/NoasF.java
@@ -22,15 +22,7 @@ public class NoasF extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NoasF.
-     */
-    // TODO: Rename and change types and number of parameters
+    
     public static NoasF newInstance(String param1, String param2) {
         NoasF fragment = new NoasF();
         Bundle args = new Bundle();
