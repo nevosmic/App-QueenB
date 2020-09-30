5a7ee6e2b2f8400c5574b5f33289f1459fa7f1c9
nevosmic <nevosmic28@gmail.com>
2020-09-30 19:55:45 +0300

    Gallery is done

--- app/src/main/java/com/example/queenb/NoasF.java
+++ app/src/main/java/com/example/queenb/NoasF.java
@@ -1,6 +1,7 @@
 package com.example.queenb;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -20,8 +21,7 @@ import com.example.queenb.main_fragment.MainFragment;
 
 public class NoasF extends Fragment {
 
-    private Button who_are_we;
-
+   // private Button who_are_we;
     public NoasF() {
         // Required empty public constructor
     }
@@ -52,12 +52,50 @@ public class NoasF extends Fragment {
        // gallery.removeAllViews();
         LayoutInflater layoutInflater = LayoutInflater.from(getContext());
 
-        for(int i=0;i<7;i++){
+//        for (int i = 1; i <= 8; i++) {
+//            View myView = layoutInflater.inflate(R.layout.item1, gallery,false);
+//            String imageName = "R.drawable.gallery" + i; // imageName is R.drawable.gallery1 / R.drawable.gallery2 and so on
+//
+//            int id = getResources().getIdentifier(imageName, "drawable", getContext().getPackageName()); // turning it into actual id no.
+//
+//            ImageView image1 = myView.findViewById(R.id.imageView1);
+//
+//           // ImageView imageView = myView.findViewById(id);
+//            image1.setImageResource(id);
+//            gallery.addView(myView);
+//
+//        }
+
+
+        for(int i=0;i<=7;i++){
                View myView = layoutInflater.inflate(R.layout.item1, gallery,false);
-                //one image for example
-                int imageSource = R.drawable.q10;
+                int a =R.drawable.gallery1;
                 ImageView image1 = myView.findViewById(R.id.imageView1);
-                image1.setImageResource(imageSource);
+                if(i==0){
+                    image1.setImageResource(a);
+                }
+                else if(i==1){
+                    image1.setImageResource(R.drawable.gallery2);
+                }
+                else if(i==2){
+                    image1.setImageResource(R.drawable.gallery3);
+                }
+                else if(i==3){
+                    image1.setImageResource(R.drawable.gallery4);
+                }
+                else if(i==4){
+                    image1.setImageResource(R.drawable.gallery5);
+                }
+                else if(i==5){
+                    image1.setImageResource(R.drawable.gallery6);
+                }
+                else if(i==6){
+                    image1.setImageResource(R.drawable.gallery7);
+                }
+                else if(i==7){
+                    image1.setImageResource(R.drawable.gallery8);
+                }
+
                 gallery.addView(myView);
             }
 
